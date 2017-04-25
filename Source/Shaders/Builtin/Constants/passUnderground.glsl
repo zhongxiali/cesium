4d8ce0a9944be9c49b9656ca08d7e18ffaad3f3d
Kevin Ring <kevin@kotachrome.com>
2017-04-25 17:21:55 +1000

    Add missing file.

---
+++ Source/Shaders/Builtin/Constants/passUnderground.glsl
@@ -0,0 +1,9 @@
+/**
+ * The automatic GLSL constant for {@link Pass#UNDEGROUND}
+ *
+ * @name czm_passUnderground
+ * @glslConstant
+ *
+ * @see czm_pass
+ */
+const float czm_passUnderground = 2.0;
